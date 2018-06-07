3c4a405351d11c1799107dfab33727fa0a6742b0
Nathan Falcone <ntfalcone93@gmail.com>
2018-06-06 18:31:23 -0600

    Setup basic PokedexTVC

---
+++ Pokedex/PokedexTableViewController.swift
@@ -0,0 +1,42 @@
+//
+//  PokedexTableViewController.swift
+//  Pokedex
+//
+//  Created by Nathan Falcone on 6/6/18.
+//  Copyright © 2018 Falcone Development. All rights reserved.
+//
+
+import UIKit
+
+class PokedexTableViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Uncomment the following line to preserve selection between presentations
+        // self.clearsSelectionOnViewWillAppear = false
+
+        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+        // self.navigationItem.rightBarButtonItem = self.editButtonItem
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    // MARK: - Table view data source
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return 0
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "pokemonCell", for: indexPath)
+
+        // Configure the cell...
+
+        return cell
+    }
+}
