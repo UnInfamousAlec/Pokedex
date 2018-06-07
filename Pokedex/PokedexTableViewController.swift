a0170f7b7f6cc574972a221e342ef301c5762e86
Nathan Falcone <ntfalcone93@gmail.com>
2018-06-06 20:08:30 -0600

    Added memory warning

--- Pokedex/PokedexTableViewController.swift
+++ Pokedex/PokedexTableViewController.swift
@@ -40,7 +40,8 @@ class PokedexTableViewController: UITableViewController {
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        
+        print("WARNING: Running low on memory.")
     }
     
     // MARK: - Table view data source
