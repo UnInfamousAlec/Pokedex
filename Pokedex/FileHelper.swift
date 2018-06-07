cdfbc8aa30790ce7e06f0830c3b46f66885d863c
Nathan Falcone <ntfalcone93@gmail.com>
2018-06-06 22:06:40 -0600

    Added FileHelper to store and retrieve images on disk

---
+++ Pokedex/FileHelper.swift
@@ -0,0 +1,54 @@
+//
+//  FileHelper.swift
+//  Pokedex
+//
+//  Created by Nathan Falcone on 6/6/18.
+//  Copyright © 2018 Falcone Development. All rights reserved.
+//
+
+import UIKit
+
+class FileHelper {
+    
+    static let fileManager = FileManager.default
+    
+    static var pokedexDirectory: URL {
+        let documentsDirectory = fileManager.urls(for: .documentDirectory, in: .userDomainMask)[0]
+        let pokedexDirectory = documentsDirectory.appendingPathComponent("Pokedex", isDirectory: true)
+        return pokedexDirectory
+    }
+    
+    static func fileUrl(for pokemon: Pokemon) -> URL {
+        let imageName = "#\(pokemon.entryNumber)\(pokemon.name).png"
+        let fileUrl = pokedexDirectory.appendingPathComponent(imageName)
+        return fileUrl
+    }
+    
+    static func store(_ image: UIImage, for pokemon: Pokemon) {
+        let fileUrl = self.fileUrl(for: pokemon)
+        
+        guard fileManager.fileExists(atPath: fileUrl.path) == false else {
+            print("Failed to store image. File exists at: \(fileUrl.path).")
+            return
+        }
+        guard let imageData = UIImageJPEGRepresentation(image, 1.0) else {
+            print("Failed to store image. Could not convert image to Data.")
+            return
+        }
+        do {
+            try imageData.write(to: fileUrl)
+        } catch {
+            print("Failed to write image to file path: \(fileUrl.path).")
+        }
+    }
+    
+    static func retrieve(imageFor pokemon: Pokemon) -> UIImage? {
+        let fileUrl = self.fileUrl(for: pokemon)
+        
+        guard let data = fileManager.contents(atPath: fileUrl.path),
+            let image = UIImage(data: data) else {
+                return nil
+        }
+        return image
+    }
+}
